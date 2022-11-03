dd849561032669f0199c4ae88556c1e99949e6ca
Aniket Patil <68179612+AniketPatil1710@users.noreply.github.com>
2022-11-03 23:16:14 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (81.08%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int c=0,res=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='R'){
+                c++;
+                if(c==0)
+                    res++;
+            }
+            else{
+                c--;
+                if(c==0)
+                    res++;
+            } 
+        }
+        return res;   
+    }
+};
