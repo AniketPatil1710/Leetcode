f860f44c77c305c5e2eb86e44a7ba934257e6e72
Aniket Patil <68179612+AniketPatil1710@users.noreply.github.com>
2026-01-16 18:50:24 +0530

    Time: 26 ms (62.76%), Space: 24.5 MB (15.04%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,55 @@
 class Solution {
+private:
+    void bfs(int i , int j , vector<vector<int>> &vis , vector<vector<char>> &grid)
+    {
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        vis[i][j] = 1 ;
+        queue<pair<int,int>> q ;
+        q.push({i,j}) ;
+
+        int dx[] = {1,0,0,-1};
+        int dy[] = {0,1,-1,0};
+        while(!q.empty())
+        {
+            auto it = q.front() ;
+            q.pop();
+            int x = it.first ;
+            int y = it.second ;
+
+            for(int k=0 ; k<4 ; ++k)
+            {
+                int dxp = dx[k]+x ;
+                int dyp = dy[k]+y ;
+
+                if(dxp>=0 and dyp>=0 and dxp<n and dyp<m and vis[dxp][dyp]==0 and grid[dxp][dyp]=='1')
+                {
+                    vis[dxp][dyp]=1;
+                    q.push({dxp,dyp});
+                }
+            }
+        }    
+    }    
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int res=0;
-        for(int i=0;i<grid.size();i++)
-            for(int j=0;j<grid[i].size();j++)
-                if(grid[i][j]=='1')// if 1, call the dfs and make res++ to add this island 
-                {
-                    dfs(grid,i,j);
-                    res++;
-                }
-        return res;
-    }
-    void dfs(vector<vector<char>>& v,int i,int j)
-    {
-        if(i<0 || j<0 || i>=v.size() || j>=v[0].size() || v[i][j]=='0')
-            return;
-        
-        v[i][j]='0';// reset to 0 as this is already included
-        dfs(v,i-1,j);
-        dfs(v,i+1,j);
-        dfs(v,i,j-1);
-        dfs(v,i,j+1);
+       
+       int n = grid.size() ;
+       int m = grid[0].size() ;
+       int ans = 0 ;
+
+       vector<vector<int>> vis(n,vector<int> (m,0));
+
+       for(int i=0 ; i<n ; ++i)
+       {
+           for(int j=0 ; j<m ; ++j)
+           {
+               if(grid[i][j]== '1'  and vis[i][j]==0)
+               {
+                   bfs(i,j,vis,grid);
+                   ans++;
+               }
+           }
+       }
+       return ans ;
     }
 };
