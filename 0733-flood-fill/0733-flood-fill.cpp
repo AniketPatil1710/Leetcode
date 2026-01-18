38349529f43b04e572fa0e59059559edb0623bcd
Aniket Patil <68179612+AniketPatil1710@users.noreply.github.com>
2026-01-18 21:48:34 +0530

    Time: 0 ms (100%), Space: 18.1 MB (55.48%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,48 @@
 class Solution {
-public:
-    void dfs(vector<vector<int>>& image, int i, int j,int val, int newColor)
+
+private :
+
+    bool check(int n , int m , int row , int col)
     {
-        if(i<0 || i>=image.size() || j<0 || j>= image[0].size() || image[i][j] == newColor || image[i][j] != val)
+        if((row>=0 && row<n) && (col>=0 && col<m))
         {
-            return;
+            return true ;
         }
-        image[i][j] = newColor;
-        dfs(image,i-1,j,val,newColor);
-        dfs(image,i+1,j,val,newColor);
-        dfs(image,i,j-1,val,newColor);
-        dfs(image,i,j+1,val,newColor);
+        return false ;
     }
-    
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor)
+
+    void dfs(int i,int j,vector<int> &dx,vector<int> &dy,vector<vector<int>> &ans ,vector<vector<int>> &image ,int in_col ,int color)
     {
-        int val = image[sr][sc];
-        dfs(image,sr,sc,val,newColor);
-        return image;
+        ans[i][j] = color ;
+
+        int n = image.size() ;
+        int m = image[0].size() ;
+
+        for(int k=0 ; k<4 ; ++k)
+        {
+            int row = i + dx[k] ;
+            int col = j + dy[k] ;
+
+            if(check(n,m,row,col) && (image[row][col] == in_col) && (ans[row][col]!=color))
+            {
+                dfs(row,col,dx,dy,ans,image,in_col,color);
+            }
+
+        }
+    }
+
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) { 
+
+        vector<vector<int>> ans = image ;
+
+        int in_col = image[sr][sc] ;
+
+        vector<int> dx = {-1,0,+1,0};
+        vector<int> dy = {0,+1,0,-1};
+
+        dfs(sr,sc,dx,dy,ans,image,in_col,color);
+
+        return ans ;
     }
 };
